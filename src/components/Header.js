601963bd153db7a7642ecdc637584f0d6af8b361
seanarch <zhuqingyun@outlook.com>
2023-06-08 11:43:02 -0600

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Container,
+  Navbar,
+  FormControl,
+  Dropdown,
+  Badge,
+  Nav,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a>Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            type="search"
+            placeholder="Search product"
+            className="m-auto"
+          ></FormControl>
+        </Navbar.Text>
+        <Nav>
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
