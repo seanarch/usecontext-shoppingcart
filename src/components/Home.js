2298669c60a3648e6299435db1336c43fc43bde0
seanarch <zhuqingyun@outlook.com>
2023-06-08 16:01:40 -0600

    Add single product

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import SingleProduct from "./SingleProduct";
 
 function Home() {
-  const { state } = CartState();
-  console.log(state);
-  return <div>Home</div>;
+  const {
+    state: { products },
+  } = CartState();
+
+  return (
+    <div className="home">
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProduct prod={prod} key={prod.id} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
