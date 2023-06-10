b03e482d251b01c3a85e07a23d007ae23a8fcd9e
seanarch <zhuqingyun@outlook.com>
2023-06-10 16:29:38 -0600

    Added sidebar, calculate total in checkout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CartState } from "../context/Context";
+import { ListGroup, Button } from "react-bootstrap";
 
 function Cart() {
-  return <div>Cart</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, [cart]);
+
+  return (
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((prod) => (
+            <span>{prod.name}</span>
+          ))}
+        </ListGroup>
+      </div>
+      <div className="filters summary">
+        <span className="title">Subtotal ({cart.length}) items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>TOtal: $ {total}</span>
+        <Button type="button" disabled={cart.length === 0}>
+          Proceed to Checkout
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
